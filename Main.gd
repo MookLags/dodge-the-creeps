a162fe522baad1a5ffd23307398ce3d248f5aa7c
Mitchell Lagaras <mookielagaras@gmail.com>
2025-08-08 16:57:18 -0400

    Fix function declarations in wrong script

---
+++ Main.gd
@@ -0,0 +1,23 @@
+extends Node
+export(PackedScene) var mob_scene
+
+var score
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$ScoreTimer.start()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
