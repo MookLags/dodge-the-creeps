5b09d9794b1fca86245fb66efc689fd14f013a97
Mitchell Lagaras <mookielagaras@gmail.com>
2025-08-08 05:50:59 -0400

    Add player motion

--- Player.gd
+++ Player.gd
@@ -5,9 +5,21 @@ var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.x -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
